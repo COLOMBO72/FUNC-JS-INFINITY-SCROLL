bf60827d0842f3d2c161c3e8028f6057001808c6
COLOMBO72 <85128844+COLOMBO72@users.noreply.github.com>
2023-11-25 16:34:02 +0700

    created function
    шаблон функции

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles.css";
+
+const Header = () => {
+    return (
+        <header>
+            <a>Главная</a>
+            <a></a>
+            <a>Главная</a>
+        </header>
+    )
+}
+
+export default Header;
